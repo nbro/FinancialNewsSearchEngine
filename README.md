de87f6693ae2b77cabba5df87e4522a59d1bc41f
nelson-brochado <nelson.brochado@outlook.com>
2016-11-29 00:00:39 +0100

    Updated readme with more info and explaining briefly the approach. updated also build.sh

--- README.md
+++ README.md
@@ -1,14 +1,40 @@
 # FinancialNewsSearchEngine
 
-## Configuration
+All of the following commands and explanations assume that you're on a unix-like system and that you have `Java` and `maven` installed. Currently I'm using `Java 1.8.0_40` and `Apache Maven 3.3.9`. If you're on Windows and you're stuck, feel free to <a href="mailto:nelson.brochado@outlook.com">contact me</a>.
 
-Before running the web app, you first need to download [`apache-nutch-2.3.1`](http://www.apache.org/dyn/closer.lua/nutch/2.3.1/apache-nutch-2.3.1-src.tar.gz), [`hbase-0.98.8-hadoop2`](https://archive.apache.org/dist/hbase/hbase-0.98.8/) and [`solr-4.10.3`](http://archive.apache.org/dist/lucene/solr/4.10.3/) (I encourage you to download all of the three programs to the folder where this README resides, i.e., at the same level as the folder `app/`).  It's possible that other versions of the above mentioned software also work together without problems of compatibility, but these versions are the [suggested ones](https://nutch.apache.org/#21-january-2016-nutch-231-release) to work together by the Nutch's creators. I decided to pick the lastest version of Nutch (2.X), which is still distributed only as source code which can be compiled, because I was interested in having some flexibility in choosing a separate database to store the crawled data, which Nutch 2.X provides quite nicely.
+Software you will _absolutely_ need to download is:
 
-Once you have those 3 programs, you should replace the files that you find in [`configuration/`](configuration) for each of the programs above with their corresponding original version. If you don't do this, "you will probably not be able to run anything"... Note that I've left the structure of the folders the same as the original, but I kept only the required folders and files.
+1. [`apache-nutch-2.3.1`](http://www.apache.org/dyn/closer.lua/nutch/2.3.1/apache-nutch-2.3.1-src.tar.gz) (source distribution, since at the time of this writing there's still no binary one)
 
-## Useful tutorials
+2. [`hbase-0.98.8-hadoop2-bin.tar.gz`](https://archive.apache.org/dist/hbase/hbase-0.98.8/)
 
-These are tutorials that I encourage you to follow before proceeding with the next section.
+3. [`solr-4.10.3.zip`](http://archive.apache.org/dist/lucene/solr/4.10.3/)
+
+4. [`ant`](http://ant.apache.org/)
+
+Other software you may _eventually_ need while reading this _READAME_ are:
+
+1. [`wget`](https://www.gnu.org/software/wget/)
+
+2. [`tar`](https://www.gnu.org/software/tar/)
+
+3. [`unzip`](http://www.info-zip.org/)
+
+4. [`tor`](https://www.torproject.org/)
+
+5. [`torsocks`](https://github.com/dgoulet/torsocks)
+
+6. [`pkill`](https://www.freebsd.org/cgi/man.cgi?query=pkill&sektion=1)
+
+Notice that you may have already some of these programs installed on your system. 
+
+If you're on a Mac OS X, you can install most of these programs using for example the package manager _Homebrew_. On Linux the package manager may be different but the process should be the same.
+
+**I've created a script [`build.sh`](./build.sh) which basically automates the tasks described in the sections *configuration* and *commands*. This script is still not robuts but in theory could avoid you to read the sections I've just mentioned and simply acess the web app at [http://127.0.0.1:3000](http://127.0.0.1:3000)**.
+
+## Tutorials
+
+These are tutorials that I encourage you to follow before proceeding with the next section, if you are not familiar with Nutch, HBase and Solr.
 
 
 - [https://wiki.apache.org/nutch/NutchTutorial](https://wiki.apache.org/nutch/NutchTutorial) (for Nutch 1.X)
@@ -18,6 +44,21 @@ These are tutorials that I encourage you to follow before proceeding with the ne
 
 - [http://hbase.apache.org/book.html#quickstart](http://hbase.apache.org/book.html#quickstart)
 
+- [http://lucene.apache.org/solr/quickstart.html](http://lucene.apache.org/solr/quickstart.html)
+
+## Help
+
+
+- [http://stackoverflow.com/search?q=nutch](http://stackoverflow.com/search?q=nutch)
+
+- [http://stackoverflow.com/search?q=solr](http://stackoverflow.com/search?q=solr)
+
+
+## Configuration
+
+Before running the web app, you first need to download [`apache-nutch-2.3.1`](http://www.apache.org/dyn/closer.lua/nutch/2.3.1/apache-nutch-2.3.1-src.tar.gz), [`hbase-0.98.8-hadoop2`](https://archive.apache.org/dist/hbase/hbase-0.98.8/) and [`solr-4.10.3`](http://archive.apache.org/dist/lucene/solr/4.10.3/) (I encourage you to download all of the three programs to the folder where this README resides, i.e., at the same level as the folder `app/`).  It's possible that other versions of the above mentioned software also work together without problems of compatibility, but these versions are the [suggested ones](https://nutch.apache.org/#21-january-2016-nutch-231-release) to work together by the Nutch's creators. I decided to pick the lastest version of Nutch (2.X), which is still distributed only as source code which can be compiled, because I was interested in having some flexibility in choosing a separate database to store the crawled data, which Nutch 2.X provides quite nicely.
+
+Once you have those 3 programs, you should replace the files that you find in [`configuration/`](configuration) for each of the programs above with their corresponding original version. If you don't do this, "you will probably not be able to run anything"... Note that I've left the structure of the folders the same as the original, but I kept only the required folders and files.
 
 ## Commands
 
@@ -72,7 +113,7 @@ After that you can finally _crawl_ data (starting from the seeds/URLs that you s
         runtime/local/bin/nutch updatedb -all
 
 
-Once that's finished, you can pass the crawled that to Solr to be indexed with the following command:
+Once that's finished, you can pass the crawled data to Solr to be indexed with the following command:
 
     runtime/local/bin/nutch solrindex http://localhost:8983/solr -all
 
@@ -80,8 +121,6 @@ After all these steps, if you haven't encountered any problems, you can go to th
 
 
 ### Web app
-
-All of the following commands and explanations assume that you're on a unix-like system and that you have `Java` and `maven` installed. Currently I'm using `Java 1.8.0_40` and `Apache Maven 3.3.9`.
 
 If you want to run this app from _Intellij IDEA_, you could have problems because of the resources managed by _wro4j_ are not downloaded.  I've not tried to run this app from `Eclipse`, but apparently there's a plug-in for Eclipse for _wro4j_, so I guess you can safely run the app from Eclipse. Anyway, I advise you to use the following commands from the terminal to run the app, which must be run from inside the folder [`app/`](./app/).
 
@@ -100,18 +139,24 @@ After the execution of the command `mvn spring-boot:run`, go to your favourite b
 
     localhost:3000
         
-## Scripts
+## Approach and Design
 
-I've created a script [`build.sh`](./build) which basically automates the tasks described in the sections "configuration" and "commands". If you have any problems running it, do not hesitate to contact me.
-        
+I've decided to use Nutch version 2 because, as I explained above, I was interested in having some flexibility in the choice of the data storage and to have some abstraction with respect to the crawling phase. Thus I also had to make a choice of the database: I chose Hbase mostly for it's strong connection with Nutch 2. These choice of course brough some tedious consequences, because I had to build Nutch from the source code.
+
+Instead of using _Lucene_ directly and having full freedom to design my app I decided to opt for the more "concrete" search platform _Solr_, which is built on top of Lucene and can run on the most used and famous Java servlet containers, such as _Jetty_ or _Tomcat_. More specifically, since I decided to use _SpringBoot_ as the web framework to develop the backend of my search web app (on top of Tomcat), I used the _Spring Data Solr_, which is a tool/library that provides already a few nice features to query _Solr_ and thus provides another level of abstraction with respect to _Solr_, even though it's of course quite connected to it. These are one of the most interesting SpringBoot's features which ease a lot the query phase to a database, so why shouldn't I use them?
+
+Regarding the configuration of Nutch and Solr, in particular the configuration of the _schema_, e.g. the configuration of the fields for each crawled and indexed document, I decided not mess much with it and to basically copy the `conf/schema.xml` file to the only Solr's core or collection (since they must be the same or at least compatible, lets say) that I'm using, that is `collection1`.
+
+I used Angular JS and Bootstrap to develop the simple front-end interface, because  I wanted to get more into Angular JS and because I had already some knowledge of Bootstrap.
+
 ## Problems
 
-This section describes some problems that I've faced while setting up Nutch, HBase, Solr, etc.
-
+This section describes some problems that I've faced while setting up Nutch, HBase, Solr, etc. In general, I found the documentation lacking of many details and updates. I encountered many problems especially when trying to combine the tools because explanations are outdated or misleading. 
 
 ### java.lang.ClassNotFoundException: org.apache.gora.hbase.store.HBaseStore
     
  - [http://stackoverflow.com/a/25830910/3924118](http://stackoverflow.com/a/25830910/3924118)
+
     
 ## Authors
 
--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# author: Nelson Brochado
-
 # this bash script was thought to run on OS X, but it can be modified easily to run also on any other unix-like system!
 
 # colors used when printing
@@ -11,115 +9,137 @@ NORMAL=$(tput sgr0)
 YELLOW=$(tput setaf 3)
 
 
-command -v wget >/dev/null 2>&1 || { echo >&2 "'wget not installed. please, install it before proceeding."; exit 1; }
-command -v tar >/dev/null 2>&1 || { echo >&2 "'tar' not installed. please, install it before proceeding."; exit 1; }
-command -v unzip >/dev/null 2>&1 || { echo >&2 "'unzip' not installed. please, install it before proceeding."; exit 1; }
-command -v tor >/dev/null 2>&1 || { echo >&2 "'tor' not installed. please, install it before proceeding."; exit 1; }
-command -v torsocks >/dev/null 2>&1 || { echo >&2 "'torsocks' not installed. please, install it before proceeding."; exit 1; }
-command -v pkill >/dev/null 2>&1 || { echo >&2 "'pkill' not installed. please, install it before proceeding."; exit 1; }
+programs_required_exist() {
+	command -v mvn >/dev/null 2>&1 || { echo >&2 "'maven' not installed. please, install it before proceeding."; exit 1; }
+	command -v wget >/dev/null 2>&1 || { echo >&2 "'wget' not installed. please, install it before proceeding."; exit 1; }
+	command -v tar >/dev/null 2>&1 || { echo >&2 "'tar' not installed. please, install it before proceeding."; exit 1; }
+	command -v unzip >/dev/null 2>&1 || { echo >&2 "'unzip' not installed. please, install it before proceeding."; exit 1; }
+	command -v tor >/dev/null 2>&1 || { echo >&2 "'tor' not installed. please, install it before proceeding."; exit 1; }
+	command -v torsocks >/dev/null 2>&1 || { echo >&2 "'torsocks' not installed. please, install it before proceeding."; exit 1; }
+	command -v pkill >/dev/null 2>&1 || { echo >&2 "'pkill' not installed. please, install it before proceeding."; exit 1; }
+}
 
-########################################## 
 
-printf "${YELLOW}checking existance of configuration directories...${NORMAL}\n"
+configuration_exists() {
+	printf "${YELLOW}checking existance of configuration directories...${NORMAL}\n"
 
-if [ ! -d "configuration/apache-nutch-2.3.1/conf" ]; then
-	printf "${RED}'Directory 'configuration/apache-nutch-2.3.1/conf' should exist with 3 files!${NORMAL}\n";
-	exit 1;
-fi
+	if [ ! -d "configuration/apache-nutch-2.3.1/conf" ]; then
+		printf "${RED}'Directory 'configuration/apache-nutch-2.3.1/conf' should exist with 3 files!${NORMAL}\n";
+		exit 1;
+	fi
 
-if [ ! -d "configuration/apache-nutch-2.3.1/ivy" ]; then
-	printf "${RED}'Directory 'configuration/apache-nutch-2.3.1/ivy' should exist with 1 file!${NORMAL}\n";
-	exit 1;
-fi
+	if [ ! -d "configuration/apache-nutch-2.3.1/ivy" ]; then
+		printf "${RED}'Directory 'configuration/apache-nutch-2.3.1/ivy' should exist with 1 file!${NORMAL}\n";
+		exit 1;
+	fi
 
-if [ ! -d "configuration/apache-nutch-2.3.1/seeds" ]; then
-	printf "${RED}'Directory 'configuration/apache-nutch-2.3.1/seeds' should exist with 1 file!${NORMAL}\n";
-	exit 1;
-fi
+	if [ ! -d "configuration/apache-nutch-2.3.1/seeds" ]; then
+		printf "${RED}'Directory 'configuration/apache-nutch-2.3.1/seeds' should exist with 1 file!${NORMAL}\n";
+		exit 1;
+	fi
 
-if [ ! -d "configuration/hbase-0.98.8-hadoop2/conf" ]; then
-	printf "${RED}Directory 'configuration/hbase-0.98.8-hadoop2/conf' should exist with 1 file!${NORMAL}\n";
-	exit 1;
-fi
+	if [ ! -d "configuration/hbase-0.98.8-hadoop2/conf" ]; then
+		printf "${RED}Directory 'configuration/hbase-0.98.8-hadoop2/conf' should exist with 1 file!${NORMAL}\n";
+		exit 1;
+	fi
 
-printf "${GREEN}ok. ${RED}note that this only checked the existance of the directories not the specific required files.${NORMAL}\n\n";
+	printf "${GREEN}ok. ${RED}note that this only checked the existance of the directories not the specific required files.${NORMAL}\n\n";
+}
 
-########################################## 
 
-printf "${YELLOW}stopping tor network...${NORMAL}\n";
-sudo pkill -f tor
-# printf "${YELLOW}starting tor network...${NORMAL}\n";
-# echo -ne '\n' | tor &
+stop_tor{
+	printf "${YELLOW}stopping tor network...${NORMAL}\n";
+	sudo pkill -f tor	
+}
 
-# printf "${YELLOW}checking if programs to download are available...${NORMAL}\n";
-# torify wget -q --spider http://www.pirbot.com/mirrors/apache/nutch/2.3.1/apache-nutch-2.3.1-src.tar.gz
-# if [ $? -ne 0 ]; then
-#     printf "${RED}http://www.pirbot.com/mirrors/apache/nutch/2.3.1/apache-nutch-2.3.1-src.tar.gz not available according to 'wget'.\n${NORMAL}";
-#     printf "${RED}you have a few options:\n\t 1. contact the provider of this script and tell him about this problem.\n\t 2. download all files and configure the project manually (procedure which is potentially described in the README file).\n\t 3. try to find an alternative URL for the corresponding file to be downloaded and modify this script accordingly.${NORMAL}\n";
-#     exit 1;
-# fi
 
-# torify wget -q --spider https://archive.apache.org/dist/hbase/hbase-0.98.8/hbase-0.98.8-hadoop2-bin.tar.gz
-# if [ $? -ne 0 ]; then
-#     printf "${RED}https://archive.apache.org/dist/hbase/hbase-0.98.8/hbase-0.98.8-hadoop2-bin.tar.gz.\n${NORMAL}";
-#     printf "${RED}you have a few options:\n\t 1. contact the provider of this script and tell him about this problem.\n\t 2. download all files and configure the project manually (procedure which is potentially described in the README file).\n\t 3. try to find an alternative URL for the corresponding file to be downloaded and modify this script accordingly.${NORMAL}\n";
-#     exit 1;
-# fi
+start_tor {
+	stop_tor
+	printf "${YELLOW}starting tor network...${NORMAL}\n";
+	echo -ne '\n' | tor &	
+}
 
-# torify wget -q --spider https://archive.apache.org/dist/lucene/solr/4.10.3/solr-4.10.3.zip
-# if [ $? -ne 0 ]; then
-#     printf "${RED}https://archive.apache.org/dist/lucene/solr/4.10.3/solr-4.10.3.zip\n${NORMAL}";
-#     printf "${RED}you have a few options:\n\t 1. contact the provider of this script and tell him about this problem.\n\t 2. download all files and configure the project manually (procedure which is potentially described in the README file).\n\t 3. try to find an alternative URL for the corresponding file to be downloaded and modify this script accordingly.${NORMAL}\n";
-#     exit 1;
-# fi
 
-# printf "${GREEN}ok.${NORMAL}\n\n";
+can_download() {
+	printf "${YELLOW}checking if programs to download are available...${NORMAL}\n";
 
-########################################## 
+	torify wget -q --spider http://www.pirbot.com/mirrors/apache/nutch/2.3.1/apache-nutch-2.3.1-src.tar.gz
+	if [ $? -ne 0 ]; then
+	    printf "${RED}http://www.pirbot.com/mirrors/apache/nutch/2.3.1/apache-nutch-2.3.1-src.tar.gz not available according to 'wget'.\n${NORMAL}";
+	    printf "${RED}you have a few options:\n\t 1. contact the provider of this script and tell him about this problem.\n\t 2. download all files and configure the project manually (procedure which is potentially described in the README file).\n\t 3. try to find an alternative URL for the corresponding file to be downloaded and modify this script accordingly.${NORMAL}\n";
+	    exit 1;
+	fi
 
-# printf "${YELLOW}starting to donwload...${NORMAL}\n"
-# rm -rf apache-nutch*
-# rm -rf hbase*
-# rm -rf solr*
-# torify wget http://www.pirbot.com/mirrors/apache/nutch/2.3.1/apache-nutch-2.3.1-src.tar.gz
-# torify wget https://archive.apache.org/dist/hbase/hbase-0.98.8/hbase-0.98.8-hadoop2-bin.tar.gz
-# torify wget https://archive.apache.org/dist/lucene/solr/4.10.3/solr-4.10.3.zip
-# printf "${GREEN}done.${NORMAL}\n"
+	torify wget -q --spider https://archive.apache.org/dist/hbase/hbase-0.98.8/hbase-0.98.8-hadoop2-bin.tar.gz
+	if [ $? -ne 0 ]; then
+	    printf "${RED}https://archive.apache.org/dist/hbase/hbase-0.98.8/hbase-0.98.8-hadoop2-bin.tar.gz.\n${NORMAL}";
+	    printf "${RED}you have a few options:\n\t 1. contact the provider of this script and tell him about this problem.\n\t 2. download all files and configure the project manually (procedure which is potentially described in the README file).\n\t 3. try to find an alternative URL for the corresponding file to be downloaded and modify this script accordingly.${NORMAL}\n";
+	    exit 1;
+	fi
 
-########################################## 
+	torify wget -q --spider https://archive.apache.org/dist/lucene/solr/4.10.3/solr-4.10.3.zip
+	if [ $? -ne 0 ]; then
+	    printf "${RED}https://archive.apache.org/dist/lucene/solr/4.10.3/solr-4.10.3.zip\n${NORMAL}";
+	    printf "${RED}you have a few options:\n\t 1. contact the provider of this script and tell him about this problem.\n\t 2. download all files and configure the project manually (procedure which is potentially described in the README file).\n\t 3. try to find an alternative URL for the corresponding file to be downloaded and modify this script accordingly.${NORMAL}\n";
+	    exit 1;
+	fi
 
-# printf "${YELLOW}starting to decompress files...${NORMAL}\n";
-# tar -xvzf apache-nutch-2.3.1-src.tar.gz
-# tar -xvzf hbase-0.98.8-hadoop2-bin.tar.gz
-# unzip -a solr-4.10.3.zip
-# rm -rf apache-nutch-2.3.1-src.tar.gz
-# rm -rf hbase-0.98.8-hadoop2-bin.tar.gz
-# rm -rf solr-4.10.3.zip
-# printf "${GREEN}done.${NORMAL}\n\n";
+	printf "${GREEN}ok.${NORMAL}\n\n";
+}
 
-########################################## 
 
-printf "${YELLOW}starting to copy configuration files to the corresponding downloaded folders...${NORMAL}\n";
+download() {
+	printf "${YELLOW}starting to donwload...${NORMAL}\n"
+	
+	rm -rf apache-nutch*
+	rm -rf hbase*
+	rm -rf solr*
+	
+	torify wget http://www.pirbot.com/mirrors/apache/nutch/2.3.1/apache-nutch-2.3.1-src.tar.gz
+	torify wget https://archive.apache.org/dist/hbase/hbase-0.98.8/hbase-0.98.8-hadoop2-bin.tar.gz
+	torify wget https://archive.apache.org/dist/lucene/solr/4.10.3/solr-4.10.3.zip
+	
+	stop_tor
 
-cp -f configuration/apache-nutch-2.3.1/conf/gora.properties apache-nutch-2.3.1/conf/gora.properties 
-cp -f configuration/apache-nutch-2.3.1/conf/nutch-site.xml apache-nutch-2.3.1/conf/nutch-site.xml
-cp -f configuration/apache-nutch-2.3.1/conf/schema.xml apache-nutch-2.3.1/conf/schema.xml 
-cp -f configuration/apache-nutch-2.3.1/ivy/ivy.xml apache-nutch-2.3.1/ivy/ivy.xml
-cp -rf configuration/apache-nutch-2.3.1/seeds/ apache-nutch-2.3.1/seeds/
+	printf "${GREEN}done.${NORMAL}\n"
+}
 
-cp -f configuration/hbase-0.98.8-hadoop2/conf/hbase-site.xml hbase-0.98.8-hadoop2/conf/hbase-site.xml
 
-# copying the Nutch's schema to be the solr schema
-cp -f configuration/apache-nutch-2.3.1/conf/schema.xml solr-4.10.3/example/solr/collection1/conf/schema.xml
+decompress() {
+	printf "${YELLOW}starting to decompress files...${NORMAL}\n";
+	
+	tar -xvzf apache-nutch-2.3.1-src.tar.gz
+	tar -xvzf hbase-0.98.8-hadoop2-bin.tar.gz
+	unzip -a solr-4.10.3.zip
+	
+	rm -rf apache-nutch-2.3.1-src.tar.gz
+	rm -rf hbase-0.98.8-hadoop2-bin.tar.gz
+	rm -rf solr-4.10.3.zip
 
-rm -rf solr-4.10.3/example/solr/collection1/data/*
+	printf "${GREEN}done.${NORMAL}\n\n";
+}
 
-printf "${GREEN}done.${NORMAL}\n\n";
 
-##########################################
+configure() {
+	printf "${YELLOW}starting to copy configuration files to the corresponding downloaded folders...${NORMAL}\n";
+	
+	cp -f configuration/apache-nutch-2.3.1/conf/gora.properties apache-nutch-2.3.1/conf/gora.properties 
+	cp -f configuration/apache-nutch-2.3.1/conf/nutch-site.xml apache-nutch-2.3.1/conf/nutch-site.xml
+	cp -f configuration/apache-nutch-2.3.1/conf/schema.xml apache-nutch-2.3.1/conf/schema.xml 
+	cp -f configuration/apache-nutch-2.3.1/ivy/ivy.xml apache-nutch-2.3.1/ivy/ivy.xml
+	cp -rf configuration/apache-nutch-2.3.1/seeds/ apache-nutch-2.3.1/seeds/
+	cp -f configuration/hbase-0.98.8-hadoop2/conf/hbase-site.xml hbase-0.98.8-hadoop2/conf/hbase-site.xml
+	# copying the Nutch's schema to be the solr schema
+	cp -f configuration/apache-nutch-2.3.1/conf/schema.xml solr-4.10.3/example/solr/collection1/conf/schema.xml
+	
+	rm -rf solr-4.10.3/example/solr/collection1/data/*
+
+	printf "${GREEN}done.${NORMAL}\n\n";
+}
+
 
 solr() {
-
+	printf "${YELLOW}starting solr...${NORMAL}\n";
 	cd solr-4.10.3
 
 	if [ ! -f "bin/solr" ]; then
@@ -130,9 +150,13 @@ solr() {
 	bin/solr start -c -dir example
 
 	cd ..
+	printf "${GREEN}done.${NORMAL}\n\n";
 }
 
+
 nutch() {
+	printf "${YELLOW}starting nutch...${NORMAL}\n";
+
 	command -v ant >/dev/null 2>&1 || { echo >&2 "'ant' not installed. please, install it before proceeding."; exit 1; }
 
 	cd apache-nutch-2.3.1
@@ -174,10 +198,12 @@ nutch() {
 	printf "${GREEN}done.${NORMAL}\n\n";
 
 	cd ..
+	printf "${GREEN}done.${NORMAL}\n\n";
 }
 
-hbase() {
 
+hbase() {
+	printf "${YELLOW}starting hbase...${NORMAL}\n";
 	cd hbase-0.98.8-hadoop2
 
 	if [ ! -f "bin/start-hbase.sh" ]; then
@@ -188,17 +214,36 @@ hbase() {
 	bin/start-hbase.sh
 
 	cd ..
+	printf "${GREEN}done.${NORMAL}\n\n";
+}
+
+run_webapp() {
+	cd app
+	mvn clean install
+	mvn spring-boot:run
+	cd ..
 }
 
 
-printf "${YELLOW}starting solr...${NORMAL}\n";
-solr
-printf "${GREEN}done.${NORMAL}\n\n";
+start() {
+	programs_required_exist
 
-printf "${YELLOW}starting hbase...${NORMAL}\n";
-hbase
-printf "${GREEN}done.${NORMAL}\n\n";
+	configuration_exists
 
-printf "${YELLOW}starting nutch...${NORMAL}\n";
-nutch
-printf "${GREEN}done.${NORMAL}\n\n";
+	can_download
+
+	download	
+
+	decompress
+
+	configure
+
+	solr
+
+	hbase
+
+	nutch
+
+	run_webapp
+}
+
